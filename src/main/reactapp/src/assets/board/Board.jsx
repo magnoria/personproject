3f4c5609c4e2481e69b305fd3e75dbbc10d9f0d2
magnoria <sidaumv@gmail.com>
2025-07-01 07:48:22 +0900

    리액트 기본 정리리

---
+++ src/main/reactapp/src/assets/board/Board.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h3>리액트 설정</h3>
+        
+      </div>
+      <h1>Vite + 리액트 설치</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
